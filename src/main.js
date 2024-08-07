fd0ec030a68340c8e7622d7e95462f49c8559bf0
Daniel <daniel@Daniels-MacBook-Pro.local>
2024-08-07 19:58:26 +0200

    feat: add (camera) movement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { k } from "./kaboomCtx";
 // import { dialogueData, scaleFactor } from "./constants";
 import { scaleFactor } from "./constants";
+import { displayDialogue, setCamScale } from "./utils";
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
     sliceY: 31,
@@ -94,6 +95,80 @@ k.scene("main", async () => {
             }
         }
     }
+
+    // Camera behaviour.
+    // Resize the camera given the size of the screen
+    setCamScale(k);
+    k.onResize(() => {
+        setCamScale(k);
+    });
+    // Move camera following character position
+    k.onUpdate(() => {
+        k.camPos(player.worldPos().x, player.worldPos().y - 100);
+    });
+
+    // Movement behaviour
+    k.onMouseDown((mouseBtn) => {
+        if (mouseBtn !== "left" || player.isInDialogue) return;
+
+        const worldMousePos = k.toWorld(k.mousePos());
+        player.moveTo(worldMousePos, player.speed);
+
+        const mouseAngle = player.pos.angle(worldMousePos);
+
+        const lowerBound = 50;
+        const upperBound = 125;
+
+        if (
+            mouseAngle > lowerBound &&
+                mouseAngle < upperBound &&
+                player.curAnim() !== "walk-up"
+        ) {
+            player.play("walk-up");
+            player.direction = "up";
+            return;
+        }
+
+        if (
+            mouseAngle < -lowerBound &&
+                mouseAngle > -upperBound &&
+                player.curAnim() !== "walk-down"
+        ) {
+            player.play("walk-down");
+            player.direction = "down";
+            return;
+        }
+
+        if (Math.abs(mouseAngle) > upperBound) {
+            player.flipX = false;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "right";
+            return;
+        }
+
+        if (Math.abs(mouseAngle) < lowerBound) {
+            player.flipX = true;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "left";
+            return;
+        }
+    });
+
+    function stopAnims() {
+        if (player.direction === "down") {
+            player.play("idle-down");
+            return;
+        }
+        if (player.direction === "up") {
+            player.play("idle-up");
+            return;
+        }
+
+        player.play("idle-side");
+    }
+
+    k.onMouseRelease(stopAnims);
+
 });
 
 // Excute the main scene
